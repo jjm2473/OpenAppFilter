5b50f7bb080fe5e73e58e5226d2dcf47c3eca90a
jjm2473 <1129525450@qq.com>
2024-03-14 14:34:44 +0800

    kmod-oaf: limit conntrack direction

--- oaf/src/app_filter.c
+++ oaf/src/app_filter.c
@@ -980,7 +980,7 @@ u_int32_t app_filter_hook_gateway_handle(struct sk_buff *skb, struct net_device 
 		return NF_ACCEPT;
 
 	ct = nf_ct_get(skb, &ctinfo);
-	if (ct == NULL || !nf_ct_is_confirmed(ct))
+	if (ct == NULL || CTINFO2DIR(ctinfo) == IP_CT_DIR_REPLY || !nf_ct_is_confirmed(ct))
 		return NF_ACCEPT;
 
 	AF_CLIENT_LOCK_R();
